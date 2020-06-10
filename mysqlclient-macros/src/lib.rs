267a0b70f4b0aeec4d90e4dc40feb351ea84f575
Julian Popescu <jpopesculian@gmail.com>
2020-06-10 13:43:20 +0200

    use 'mysql_get_client_info' to retrieve the version string

--- mysqlclient-macros/src/lib.rs
+++ mysqlclient-macros/src/lib.rs
@@ -1,22 +1,23 @@
 extern crate proc_macro;
 
+use mysqlclient_bindings as bindings;
 use proc_macro::{TokenStream, TokenTree};
 use semver::{Version, VersionReq};
 use std::ffi::CStr;
 
-const VERSION: &[u8] = mysqlclient_bindings::LIBMYSQL_VERSION;
 const ATTR_ERR_MSG: &str = "Expected a valid semantic version requirement string as \
                             argument to `mysqlclient_version`";
 
+fn get_version() -> Version {
+    unsafe { CStr::from_ptr(bindings::mysql_get_client_info()) }
+        .to_str()
+        .unwrap()
+        .parse()
+        .expect("`mysql_get_client_info` did not return a parseable version")
+}
+
 #[proc_macro_attribute]
 pub fn mysqlclient_version(attr: TokenStream, item: TokenStream) -> TokenStream {
-    let mysqlclient_ver = Version::parse(
-        CStr::from_bytes_with_nul(VERSION)
-            .unwrap()
-            .to_str()
-            .unwrap(),
-    )
-    .unwrap();
     if let Some(TokenTree::Literal(literal)) = attr.into_iter().next() {
         let literal_string = literal.to_string();
         if literal_string.len() < 2 {
@@ -24,7 +25,7 @@ pub fn mysqlclient_version(attr: TokenStream, item: TokenStream) -> TokenStream 
         }
         let req =
             VersionReq::parse(&literal_string[1..literal_string.len() - 1]).expect(ATTR_ERR_MSG);
-        if req.matches(&mysqlclient_ver) {
+        if req.matches(&get_version()) {
             item
         } else {
             TokenStream::new()
